e4bc2899d58b6794f65ff852de213dff2b285748
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-07-04 09:52:10 +0800

    upgrade Avalonia from rc1 to rc2;

--- src/CodeWF.Tools.Module.Test/Views/AvaPlotView.axaml.cs
+++ src/CodeWF.Tools.Module.Test/Views/AvaPlotView.axaml.cs
@@ -1,8 +1,4 @@
 using Avalonia.Markup.Xaml;
-using ScottPlot;
-using ScottPlot.Avalonia;
-using ScottPlot.DataSources;
-using ScottPlot.Plottables;
 
 namespace CodeWF.Tools.Module.Test.Views;
 
@@ -11,19 +7,6 @@ public partial class AvaPlotView : UserControl
     public AvaPlotView()
     {
         InitializeComponent();
-
-        AvaPlot avaPlot1 = this.Find<AvaPlot>("AvaPlot1");
-
-        const int lineCount = 5;
-        for (int i = 0; i < lineCount; i++)
-        {
-            var x = Enumerable.Range(0, 10).Select(index => index + 1.0).ToArray();
-            var y = Enumerable.Range(0, 10).Select(index => Random.Shared.Next(1, 20) + 1.0).ToList();
-            var logger = new DataStreamer(avaPlot1.Plot, x);
-            avaPlot1.Plot.Add.Plottable(logger);
-        }
-
-        avaPlot1.Refresh();
     }
 
     private void InitializeComponent()
