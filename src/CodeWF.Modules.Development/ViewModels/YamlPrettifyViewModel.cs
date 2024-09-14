1f433d5497878c6fe39f6495f76399609870cc36
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-09-14 16:17:28 +0800

    add yaml prettify;

--- src/CodeWF.Modules.Development/ViewModels/YamlPrettifyViewModel.cs
+++ src/CodeWF.Modules.Development/ViewModels/YamlPrettifyViewModel.cs
@@ -1,7 +1,91 @@
-﻿using ReactiveUI;
+﻿using AvaloniaEdit;
+using ReactiveUI;
+using System.Reactive.Linq;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace CodeWF.Modules.Development.ViewModels;
 
-internal class YamlPrettifyViewModel : ReactiveObject
+public class YamlPrettifyViewModel : ReactiveObject
 {
+    private IDeserializer? _deserializer;
+    private ISerializer? _serializer;
+    public TextEditor YamlTextEditor { get; set; }
+
+    public YamlPrettifyViewModel()
+    {
+        this.WhenAnyValue(x => x.RawYaml)
+            .Throttle(TimeSpan.FromMilliseconds(400))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(RawYamlChanged);
+    }
+
+    private bool _isSortKey = false;
+
+    public bool IsSortKey
+    {
+        get => _isSortKey;
+        set => this.RaiseAndSetIfChanged(ref _isSortKey, value);
+    }
+
+    public List<int> IndentSizes { get; } = Enumerable.Range(1, 10).ToList();
+
+    private int _indentSize = 2;
+
+    public int IndentSize
+    {
+        get => _indentSize;
+        set => this.RaiseAndSetIfChanged(ref _indentSize, value);
+    }
+
+    private string? _rawYaml;
+
+    public string? RawYaml
+    {
+        get => _rawYaml;
+        set => this.RaiseAndSetIfChanged(ref _rawYaml, value);
+    }
+
+    private string? _prettifiedYaml;
+
+    public string? PrettifiedYaml
+    {
+        get => _prettifiedYaml;
+        set => this.RaiseAndSetIfChanged(ref _prettifiedYaml, value);
+    }
+
+    private string? _errorMessage;
+
+    public string? ErrorMessage
+    {
+        get => _errorMessage;
+        set => this.RaiseAndSetIfChanged(ref _errorMessage, value);
+    }
+
+    private void RawYamlChanged(string? newRawYaml)
+    {
+        if (string.IsNullOrWhiteSpace(newRawYaml))
+        {
+            YamlTextEditor.Text = default;
+            return;
+        }
+
+        _deserializer ??= new DeserializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .Build();
+
+        _serializer ??= new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+
+        try
+        {
+            var obj = _deserializer.Deserialize(RawYaml);
+            YamlTextEditor.Text = _serializer.Serialize(obj);
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = ex.Message;
+        }
+    }
 }
--- src/CodeWF.Modules.Development/Views/YamlPrettifyView.axaml.cs
+++ src/CodeWF.Modules.Development/Views/YamlPrettifyView.axaml.cs
@@ -1,5 +1,9 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaEdit;
+using AvaloniaEdit.TextMate;
+using CodeWF.Modules.Development.ViewModels;
+using TextMateSharp.Grammars;
 
 namespace CodeWF.Modules.Development.Views;
 
@@ -8,6 +12,20 @@ public partial class YamlPrettifyView : UserControl
     public YamlPrettifyView()
     {
         InitializeComponent();
+
+        //First of all you need to have a reference for your TextEditor for it to be used inside AvaloniaEdit.TextMate project.
+        var textEditor = this.FindControl<TextEditor>("Editor");
+
+        //Here we initialize RegistryOptions with the theme we want to use.
+        var registryOptions = new RegistryOptions(ThemeName.DarkPlus);
+
+        //Initial setup of TextMate.
+        var textMateInstallation = textEditor.InstallTextMate(registryOptions);
+
+        //Here we are getting the language by the extension and right after that we are initializing grammar with this language.
+        //And that's all ??, you are ready to use AvaloniaEdit with syntax highlighting!
+        textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId(registryOptions.GetLanguageByExtension(".cs").Id));
+        ((DataContext as YamlPrettifyViewModel)!).YamlTextEditor = textEditor;
     }
 
     private void InitializeComponent()
