11c0e8e6d9dcd92c961ea6fb461a33ce310a2a5e
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-09-19 16:12:35 +0800

    test CodeWF.NetWeaver AOT;

---
+++ tests/ConsoleAotDemo/Dtos/Person.cs
@@ -0,0 +1,16 @@
+﻿using CodeWF.NetWeaver.Base;
+
+namespace ConsoleAotDemo.Dtos;
+
+[NetHead(10, 1)]
+public class Person : INetObject
+{
+    public string? Name { get; set; }
+
+    public decimal Property { get; set; }
+
+    public override string ToString()
+    {
+        return $"姓名：{Name}\r\n家产：{Property}万￥";
+    }
+}
--- tests/ConsoleAotDemo/Program.cs
+++ tests/ConsoleAotDemo/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("你是帅哥!");
+﻿using CodeWF.NetWeaver;
+using CodeWF.NetWeaver.Base;
+using ConsoleAotDemo.Dtos;
+
+var student = new Person { Name = "帅哥", Property = 100 };
+
+Console.WriteLine($"帅哥信息如下：\r\n{student}");
+
+var buffer = student.Serialize(1);
+Console.WriteLine($"名下：{buffer?.Length}套房");
+
+int index = 0;
+if (buffer?.ReadHead(ref index, out NetHeadInfo head) == true)
+{
+    Console.WriteLine($"哇，原来你是第{head.ObjectVersion}大帅哥");
+}
+else
+{
+    Console.WriteLine("抱歉，无法详细了解你的帅");
+}
+
 Console.ReadLine();
