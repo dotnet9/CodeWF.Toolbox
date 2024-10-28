357261e74ee3532959f227b3b133a38c0b5cacc6
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-10-28 20:25:46 +0800

    Update ChatGptOptions.cs

--- src/CodeWF.Modules.AI/Models/ChatGptOptions.cs
+++ src/CodeWF.Modules.AI/Models/ChatGptOptions.cs
@@ -4,19 +4,9 @@ namespace CodeWF.Modules.AI.Models;
 
 internal class ChatGptOptions
 {
-    private static string _baseUrl;
-    static ChatGptOptions()
-    {
-        if (Debugger.IsAttached)
-        {
-            _baseUrl = "http://localhost:5242/";
-        }
-        else
-        {
-            _baseUrl = "https://api.dotnet9.com/";
-        }
-    }
-    public string AskBotHttpUrl { get; set; } = $"{_baseUrl}ai/askbot";
-    public string PolyTranslateHttpUrl { get; set; } = $"{_baseUrl}ai/polytranslate";
-    public string Title2SlugHttpUrl { get; set; } = $"{_baseUrl}ai/title2slug";
+    private const string BaseUrl = "https://dotnet9.com/";
+
+    public string AskBotHttpUrl { get; set; } = $"{BaseUrl}ai/askbot";
+    public string PolyTranslateHttpUrl { get; set; } = $"{BaseUrl}ai/polytranslate";
+    public string Title2SlugHttpUrl { get; set; } = $"{BaseUrl}ai/title2slug";
 }
