a7a7464d0e74ab99a6d87413a6f4d4b14d732dff
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-07-28 19:32:30 +0800

    Update AppBuilderExtension.cs

--- src/CodeWF.Toolbox/AppBuilderExtension.cs
+++ src/CodeWF.Toolbox/AppBuilderExtension.cs
@@ -6,11 +6,11 @@ public static class AppBuilderExtension
 {
     public static AppBuilder WithFont_SourceHanSansCN(this AppBuilder appBuilder)
     {
-        var uri = "avares://CodeWF.Toolbox/Assets/Fonts#Source Han Sans CN";
+        const string uri = "avares://CodeWF.Toolbox/Assets/Fonts#Source Han Sans CN";
         return appBuilder.With(new FontManagerOptions()
         {
             DefaultFamilyName = uri,
-            FontFallbacks = new[] { new FontFallback { FontFamily = new FontFamily(uri) } }
+            FontFallbacks = [new FontFallback { FontFamily = new FontFamily(uri) }]
         });
     }
 }
